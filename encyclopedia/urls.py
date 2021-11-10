3c2e1b6322b12b2d1be9c7774805128c889ded94
Etentuk <etentuke@gmail.com>
2021-11-10 20:00:49 +0100

    completed search specifications

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("search", views.search_results, name="search_results"),
     path("<str:title>", views.entry_page, name="entry_page"),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,7 +1,14 @@
 import re
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django import forms
+from django.urls.base import resolve
 from . import util
+from django.http.response import HttpResponseRedirect
+# from django.urls import reverse
+
+
+class SearchForm(forms.Form):
+    q = forms.CharField()
 
 
 def index(request):
@@ -13,5 +20,22 @@ def index(request):
 def entry_page(request, title):
     return render(request, "encyclopedia/entry.html", {
         "title": title.capitalize(),
-        "entry": util.get_entry(title)
+        "entry": util.get_entry(title),
     })
+
+
+def search_results(request):
+    if request.method == "POST":
+        form = SearchForm(request.POST)
+        entries = util.list_entries()
+        if form.is_valid():
+            query = form.cleaned_data["q"]
+            matching_entries = []
+            for entry in entries:
+                if entry.lower() == query.lower():
+                    return entry_page(request, query)
+                elif query.lower() in entry.lower():
+                    matching_entries += [entry]
+        return render(request, "encyclopedia/search.html", {
+            "matching_entries": matching_entries
+        })
